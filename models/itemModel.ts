206326990d9e8581063b717eada4b1f4a8a32632
Mike <33.mihail@gmail.com>
2024-09-07 16:03:53 +0300

    Added the rest of the fields to the Item schema

--- models/itemModel.ts
+++ models/itemModel.ts
@@ -1,12 +1,33 @@
 import { Types, Document, Schema, model, models } from "mongoose";
 
+enum MeasurementUnit {
+  gram,
+  kilogram,
+  pound,
+}
+
 export interface IItem extends Document {
   _id: Types.ObjectId;
   name: String;
+  weight: Number;
+  measurementUnit: String;
+  quantity: Number;
+  isWorn: Boolean;
 }
 
 export const itemSchema = new Schema<IItem>(
-  { name: { type: String, required: false } },
+  {
+    name: { type: String, required: false },
+    weight: { type: Number, required: false },
+    measurementUnit: {
+      type: String,
+      enum: Object.values(MeasurementUnit),
+      required: false,
+      default: MeasurementUnit.kilogram,
+    },
+    quantity: { type: Number, required: false },
+    isWorn: { type: Boolean, required: false, default: false },
+  },
   { collection: "item" }
 );
 
